cd647504ba236c7ef999819cdfa9e890d2391e02
Alex <alittle1092@gmail.com>
2020-05-10 13:44:09 +1000

    Commit for deployment

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
+  before_action :set_apparel, only: [:show]
+  before_action :set_sneakers, only: [:show]
   def index
+    @sneakers = Sneaker.all
+    @apparels = Apparel.all
+
   end
 
   def about
