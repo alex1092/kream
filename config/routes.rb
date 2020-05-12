660377c8be1e4008140f647c081b11579432ce1a
Alex <alittle1092@gmail.com>
2020-05-12 16:47:07 +1000

    Styling devise pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :sneakers
   root 'pages#index'
   get 'pages/contact', as: 'contact'
+  get 'pages/admin', as: 'admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
