7da7772e70083154eb1f0d830de963972d3a39b4
Alex <alittle1092@gmail.com>
2020-05-14 11:04:56 +1000

    Fixing credentials

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,9 @@ class OrdersController < ApplicationController
   def new
     @product = Product.find(params[:product_id])
 
-    # Rails.application.credentials.dig(:stripe, :secret_key)
+    # 
 
-    Stripe.api_key = 'sk_test_gHtLtEw1C4u1ow3oj6j4dEfS00gGLVjd2p'
+    Stripe.api_key = Rails.application.credentials.dig(:stripe, :secret_key)
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ["card"],
       line_items: [{
@@ -18,11 +18,14 @@ class OrdersController < ApplicationController
         quantity: 1,
       }],
       mode: "payment",
-      success_url: "https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: "https://example.com/cancel",
+      # success_url: "https://example.com/success?session_id={CHECKOUT_SESSION_ID}",
+      # cancel_url: "https://example.com/cancel",
 
-      # success_url: "http://http://localhost:3000/orders/complete",
-      # cancel_url: "http://http://localhost:3000/orders/cancel",
+      success_url: "http://localhost:3000/orders/complete",
+      cancel_url: "http://localhost:3000/orders/cancel",
+
+      # success_url: Rails.application.credentials.dig(:stripe, :success_url),
+      # cancel_url:  Rails.application.credentials.dig(:stripe, :cancel_url),
     )
     puts @session
   end
