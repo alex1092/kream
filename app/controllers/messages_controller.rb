bb7ef1e7d7cea397371acfb267e41ed9d2471357
Alex <alittle1092@gmail.com>
2020-05-15 14:18:29 +1000

    Created messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,5 +10,16 @@ class MessagesController < ApplicationController
 
     def new
         @message = @conversation.messages.new
+    end
 
+    def create
+        @message = @conversation.message.new(message_params)
+        if @message.save 
+            redirect_to conversation_messages_path(@conversation)
+        end
+    end
+
+    private
+    def message_params
+        params.require(:message).permit(:body, :user_id)
 end
