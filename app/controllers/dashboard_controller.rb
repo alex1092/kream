67d11491a10cbdb2def64817fdbe3efdff414d7e
Alex <alittle1092@gmail.com>
2020-05-18 15:42:01 +1000

    arranging files

--- app/controllers/dashboard_controller.rb
+++
@@ -1,5 +0,0 @@
-class DashboardController < ApplicationController
-  def index
-    @products = Product.all
-  end
-end
--- db/migrate/20200515072242_drop_phone_from_users.rb
+++
@@ -1,5 +0,0 @@
-class DropPhoneFromUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :users, :phone
-  end
-end
