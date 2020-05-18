609f61665fe5edf67f7f6862b06e9365dee3650a
Alex <alittle1092@gmail.com>
2020-05-18 15:39:26 +1000

    Arranging files

--- app/models/product.rb
+++
@@ -1,5 +0,0 @@
-class Product < ApplicationRecord
-    belongs_to :user
-    has_one_attached :picture
-    validates :category, :brand, :style, :size, :price, :picture, presence: true
-end
--- app/views/apparels/_apparel.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! apparel, :id, :brand, :model, :size, :price, :created_at, :updated_at
-json.url apparel_url(apparel, format: :json)
